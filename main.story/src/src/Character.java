671bb3f65d0aa6f83a1bac3a8c432e9b08158dc2
upura <upura0@gmail.com>
2017-12-29 12:29:35 +0900

    Create Character Class

---
+++ main.story/src/src/Character.java
@@ -0,0 +1,20 @@
+package src;
+
+public class Character {
+	private String name;
+	private String gender;
+
+	public Character(String name, String gender) {
+		this.name = name;
+		this.gender = gender;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+}
