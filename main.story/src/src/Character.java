89ac495910d614388d3a7b032686c99c20cc03cc
upura <upura0@gmail.com>
2017-12-29 14:26:38 +0900

    Add HP, MP, power and defense

--- main.story/src/src/Character.java
+++ main.story/src/src/Character.java
@@ -4,6 +4,10 @@ public class Character {
 	private String name;
 	private String gender;
 	private Weapon weapon;
+	private int HP;
+	private int MP;
+	private int power;
+	private int defense;
 
 	public Character(String name, String gender) {
 		this.name = name;
@@ -24,5 +28,37 @@ public class Character {
 
 	public Weapon getWeapon() {
 		return weapon;
+	}
+
+	public void setHP(int HP) {
+		this.HP = HP;
+	}
+
+	public int getHP() {
+		return HP;
+	}
+
+	public void setMP(int MP) {
+		this.HP = MP;
+	}
+
+	public int getMP() {
+		return MP;
+	}
+
+	public void setPower(int power) {
+		this.power = power;
+	}
+
+	public int getPower() {
+		return power;
+	}
+
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+	public int getDefense() {
+		return defense;
 	}
 }
