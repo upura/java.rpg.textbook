9825911b6a7a2ca3ac96126aef463c71b0b6c6d6
upura <upura0@gmail.com>
2017-12-29 13:42:45 +0900

    Extends abstract class [2 questions remain]

--- main.story/src/src/Claymore.java
+++ main.story/src/src/Claymore.java
@@ -1,7 +1,15 @@
 package src;
 
 public class Claymore extends Weapon{
+	private int power = 40;
+
+	// 何これ？
 	public Claymore() {
-		setWeaponName("クレイモア");
+		super("クレイモア");
+	}
+
+	// this. 要る？
+	public int getPower() {
+		return this.power;
 	}
 }
