4225a29514468ff73d451b2f8cf7b714723a88fa
upura <upura0@gmail.com>
2017-12-29 13:50:48 +0900

    Edit to test Claymore.java

--- main.story/src/src/WeaponCall.java
+++ main.story/src/src/WeaponCall.java
@@ -5,5 +5,6 @@ public class WeaponCall {
 	public static void main(String[] args) {
 		Claymore claymore = new Claymore();
 		System.out.println(claymore.getWeaponName());
+		System.out.println(claymore.getPower());
 	}
 }
