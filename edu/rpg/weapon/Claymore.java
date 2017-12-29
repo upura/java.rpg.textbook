521bb86a25a25356c8a445dc324755d2713c2421
upura <upura0@gmail.com>
2017-12-29 15:24:46 +0900

    Use enum for WeaponType

--- edu/rpg/weapon/Claymore.java
+++ edu/rpg/weapon/Claymore.java
@@ -5,7 +5,7 @@ public class Claymore extends Weapon{
 
 	// Call parent constracter by super()
 	public Claymore() {
-		super("クレイモア");
+		super(WeaponType.SWORD, "クレイモア");
 	}
 
 	// this. は無くても動く（付けた方が子インスタンス特有の変数だと分かりやすい）
--- edu/rpg/weapon/Weapon.java
+++ edu/rpg/weapon/Weapon.java
@@ -1,14 +1,20 @@
 package weapon;
 
 public abstract class Weapon {
+	private WeaponType weaponType;
 	private String weaponName;
 
-	public Weapon(String weaponName) {
+	public Weapon(WeaponType weaponType, String weaponName) {
+		this.weaponType = weaponType;
 		this.weaponName = weaponName;
 	}
 
 	public String getWeaponName() {
 		return weaponName;
+	}
+
+	public WeaponType getWeaponType() {
+		return weaponType;
 	}
 
 	public abstract int getPower();
