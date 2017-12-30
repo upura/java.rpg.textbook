c9356fe66116923c02eb7b35b6b0b62e57e53385
upura <upura0@gmail.com>
2017-12-30 13:42:19 +0900

    Change Battle.java to use Array

--- edu/rpg/battle/Battle.java
+++ edu/rpg/battle/Battle.java
@@ -3,74 +3,90 @@ import monster.Monster;
 import personal.Character;
 public class Battle {
 	private Character character;
-	private Monster monster;
-	public Battle(Character character, Monster monster) {
+	private Monster[] monsters;
+	public Battle(Character character, Monster[] monsters) {
 		this.character = character;
-		this.monster = monster;
+		this.monsters = monsters;
 	}
 	public void battle() {
-		BattlePattern pattern = this.monster.getBattlePattern();
-		if (pattern == BattlePattern.ONE) {
-			if (this.character.getAttackPower() >= this.monster.getHp()) {
-				System.out.println(this.monster.getName() + "を倒した！");
-				System.out.println(this.character.getName() + "は　経験値："
-						+ this.monster.getExp() + "ポイントを手に入れた");
-				this.character.addExp(this.monster.getExp());
-			} else if (this.monster.getAttackPower() >= this.character.getHp()) {
-				System.out.println(this.character.getName() + "は死んでしまった。");
-			} else {
-				System.out.println(this.monster.getName() + "は逃げだした！");
+		for (Monster monster : this.monsters) {
+			BattlePattern pattern = monster.getBattlePattern();
+			switch (pattern) {
+			case ONE:
+				one(this.character, monster);
+				break;
+			case THREE_TIME:
+				threeTime(this.character, monster);
+				break;
+			case ROOP:
+				roop(this.character, monster);
+				break;
 			}
-		} else if (pattern == BattlePattern.THREE_TIME) {
-			int characterHp = this.character.getHp();
-			int monsterHp = this.monster.getHp();
-			for (int i = 0; i < 3; i++) {
-				System.out.println(this.character.getName() + "の攻撃！");
-				System.out.println(this.monster.getName() + "は"
-						+ this.character.getAttackPower() + "のダメージを受けた！");
-				monsterHp -= this.character.getAttackPower();
-				if (monsterHp <= 0) {
-					System.out.println(this.monster.getName() + "を倒した！");
-					System.out.println(this.character.getName() + "は　経験値："
-							+ this.monster.getExp() + "ポイントを手に入れた");
-					this.character.addExp(this.monster.getExp());
-					break;
-				}
-				System.out.println(this.monster.getName() + "の攻撃！");
-				System.out.println(this.character.getName() + "は"
-						+ this.monster.getAttackPower() + "のダメージを受けた！");
-				characterHp -= this.monster.getAttackPower();
-				if (characterHp <= 0) {
-					System.out.println(this.character.getName() + "は死んでしまった。");
-					break;
-				}
+		}
+	}
+	private void one(Character character, Monster monster) {
+		if (character.getAttackPower() >= monster.getHp()) {
+			System.out.println(monster.getName() + "を倒した！");
+			System.out.println(character.getName() + "は　経験値："
+					+ monster.getExp() + "ポイントを手に入れた");
+			character.addExp(monster.getExp());
+		} else if (monster.getAttackPower() >= character.getHp()) {
+			System.out.println(character.getName() + "は死んでしまった。");
+		} else {
+			System.out.println(monster.getName() + "は逃げだした！");
+		}
+	}
+	private void threeTime(Character character, Monster monster) {
+		int characterHp = character.getHp();
+		int monsterHp = monster.getHp();
+		for (int i = 0; i < 3; i++) {
+			System.out.println(character.getName() + "の攻撃！");
+			System.out.println(monster.getName() + "は"
+					+ character.getAttackPower() + "のダメージを受けた！");
+			monsterHp -= character.getAttackPower();
+			if (monsterHp <= 0) {
+				System.out.println(monster.getName() + "を倒した！");
+				System.out.println(character.getName() + "は　経験値："
+						+ monster.getExp() + "ポイントを手に入れた");
+				this.character.addExp(monster.getExp());
+				break;
 			}
-			this.character.setHp(characterHp);
-			if (characterHp > 0 && monsterHp > 0) {
-				System.out.println(this.monster.getName() + "は逃げだした！");
+			System.out.println(monster.getName() + "の攻撃！");
+			System.out.println(character.getName() + "は"
+					+ monster.getAttackPower() + "のダメージを受けた！");
+			characterHp -= monster.getAttackPower();
+			if (characterHp <= 0) {
+				System.out.println(character.getName() + "は死んでしまった。");
+				break;
 			}
-		} else if (pattern == BattlePattern.ROOP) {
-			int characterHp = this.character.getHp();
-			int monsterHp = this.monster.getHp();
-			while (characterHp > 0 || monsterHp > 0) {
-				System.out.println(this.character.getName() + "の攻撃！");
-				System.out.println(this.monster.getName() + "は"
-						+ this.character.getAttackPower() + "のダメージを受けた！");
-				monsterHp -= this.character.getAttackPower();
-				if (monsterHp <= 0) {
-					System.out.println(this.monster.getName() + "を倒した！");
-					System.out.println(this.character.getName() + "は　経験値："
-							+ this.monster.getExp() + "ポイントを手に入れた");
-					this.character.addExp(this.monster.getExp());
-					break;
-				}
-				System.out.println(this.monster.getName() + "の攻撃！");
-				System.out.println(this.character.getName() + "は"
-						+ this.monster.getAttackPower() + "のダメージを受けた！");
-				characterHp -= this.monster.getAttackPower();
-				if (characterHp <= 0) {
-					System.out.println(this.character.getName() + "は死んでしまった。");
-				}
+		}
+		character.setHp(characterHp);
+		if (characterHp > 0 && monsterHp > 0) {
+			System.out.println(monster.getName() + "は逃げだした！");
+		}
+	}
+	private void roop(Character character, Monster monster) {
+		int characterHp = character.getHp();
+		int monsterHp = monster.getHp();
+		while (characterHp > 0 || monsterHp > 0) {
+			System.out.println(character.getName() + "の攻撃！");
+			System.out.println(monster.getName() + "は"
+					+ character.getAttackPower() + "のダメージを受けた！");
+			monsterHp -= character.getAttackPower();
+			if (monsterHp <= 0) {
+				System.out.println(monster.getName() + "を倒した！");
+				System.out.println(character.getName() + "は　経験値："
+						+ monster.getExp() + "ポイントを手に入れた");
+				character.addExp(monster.getExp());
+				break;
+			}
+			System.out.println(monster.getName() + "の攻撃！");
+			System.out.println(character.getName() + "は"
+					+ monster.getAttackPower() + "のダメージを受けた！");
+			characterHp -= monster.getAttackPower();
+			if (characterHp <= 0) {
+				System.out.println(character.getName() + "は死んでしまった。");
+				break;
 			}
 		}
 	}
