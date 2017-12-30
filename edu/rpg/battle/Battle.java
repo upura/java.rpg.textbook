5a10bce5f9d4a0d8324467a68a390f00346bc0b6
upura <upura0@gmail.com>
2017-12-30 13:15:39 +0900

    Add method for THREE_TIME

--- edu/rpg/battle/Battle.java
+++ edu/rpg/battle/Battle.java
@@ -1,5 +1,4 @@
 package battle;
-
 import monster.Monster;
 import personal.Character;
 public class Battle {
@@ -20,6 +19,34 @@ public class Battle {
 			} else if (this.monster.getAttackPower() >= this.character.getHp()) {
 				System.out.println(this.character.getName() + "は死んでしまった。");
 			} else {
+				System.out.println(this.monster.getName() + "は逃げだした！");
+			}
+		} else if (pattern == BattlePattern.THREE_TIME) {
+			int characterHp = this.character.getHp();
+			int monsterHp = this.monster.getHp();
+			for (int i = 0; i < 3; i++) {
+				System.out.println(this.character.getName() + "の攻撃！");
+				System.out.println(this.monster.getName() + "は"
+						+ this.character.getAttackPower() + "のダメージを受けた！");
+				monsterHp -= this.character.getAttackPower();
+				if (monsterHp <= 0) {
+					System.out.println(this.monster.getName() + "を倒した！");
+					System.out.println(this.character.getName() + "は　経験値："
+							+ this.monster.getExp() + "ポイントを手に入れた");
+					this.character.addExp(this.monster.getExp());
+					break;
+				}
+				System.out.println(this.monster.getName() + "の攻撃！");
+				System.out.println(this.character.getName() + "は"
+						+ this.monster.getAttackPower() + "のダメージを受けた！");
+				characterHp -= this.monster.getAttackPower();
+				if (characterHp <= 0) {
+					System.out.println(this.character.getName() + "は死んでしまった。");
+					break;
+				}
+			}
+			this.character.setHp(characterHp);
+			if (characterHp > 0 && monsterHp > 0) {
 				System.out.println(this.monster.getName() + "は逃げだした！");
 			}
 		}
